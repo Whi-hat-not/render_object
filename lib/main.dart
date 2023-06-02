b8c6578a7f554459f1dad450d1ea6b575008b644
enact <parikshitsharma219@gmail.com>
2023-06-02 16:14:53 +0530

    own_try_commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,265 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:flutter/semantics.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ChatAppConversationView());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const ChatAppConversationView());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class ChatAppConversationView extends StatefulWidget {
+  const ChatAppConversationView({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<ChatAppConversationView> createState() =>
+      _ChatAppConversationViewState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ChatAppConversationViewState extends State<ChatAppConversationView> {
+  final TextEditingController _controller = TextEditingController();
+  final String sentAt = "3 seconds ago";
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _controller.text = "Hello, World!";
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: SizedBox(
+          width: 220,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              (_controller.text != "")
+                  ? Align(
+                      alignment: Alignment.centerRight,
+                      child: Container(
+                        color: Colors.blue[100]!,
+                        padding: const EdgeInsets.all(15),
+                        child: ValueListenableBuilder(
+                          valueListenable: _controller,
+                          builder:
+                              (BuildContext context, value, Widget? child) {
+                            return TimestampedChatMessage(
+                                text: _controller.text,
+                                sentAt: '2 minutes ago',
+                                style: const TextStyle(color: Colors.red));
+                          },
+                        ),
+                      ),
+                    )
+                  : Container(),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 25),
+                child: TextField(
+                  controller: _controller,
+                ),
+              )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class TimestampedChatMessage extends LeafRenderObjectWidget {
+  const TimestampedChatMessage(
+      {super.key,
+      required this.text,
+      required this.sentAt,
+      required this.style});
+  final String text;
+  final String sentAt;
+  final TextStyle style;
+
+  @override
+  RenderObject createRenderObject(BuildContext context) {
+    return TimestampedChatMessageRenderObject(
+      text: text,
+      sentAt: sentAt,
+      style: style,
+      textDirection: Directionality.of(context),
+    );
+  }
+
+  @override
+  void updateRenderObject(
+      BuildContext context, TimestampedChatMessageRenderObject renderObject) {
+    renderObject.text = text;
+    renderObject.sentAt = sentAt;
+    renderObject.style = style;
+    renderObject.textDirection = Directionality.of(context);
+  }
+}
+
+class TimestampedChatMessageRenderObject extends RenderBox {
+  TimestampedChatMessageRenderObject({
+    required String text,
+    required String sentAt,
+    required TextStyle style,
+    required TextDirection textDirection,
+  }) {
+    _text = text;
+    _sentAt = sentAt;
+    _style = style;
+    _textDirection = textDirection;
+    _textPainter =
+        TextPainter(text: textTextSpan, textDirection: _textDirection);
+    _sentAtTextPainter =
+        TextPainter(text: sentAtTextSpan, textDirection: _textDirection);
+  }
+  late String _text;
+  late String _sentAt;
+  late TextStyle _style;
+  late TextDirection _textDirection;
+  late TextPainter _textPainter;
+  late TextPainter _sentAtTextPainter;
+
+  String get text => _text;
+  set text(String val) {
+    if (val == _text) {
+      return;
+    }
+    val = _text;
+    _textPainter.text = textTextSpan;
+    markNeedsLayout();
+    markNeedsSemanticsUpdate();
+  }
+
+  String get sentAt => _text;
+  set sentAt(String val) {
+    if (val == _sentAt) {
+      return;
+    }
+    val = _sentAt;
+    _sentAtTextPainter.text = sentAtTextSpan;
+    markNeedsLayout();
+    markNeedsSemanticsUpdate();
+  }
+
+  TextStyle get style => _style;
+  set style(TextStyle val) {
+    if (val == _style) {
+      return;
+    }
+    _style = val;
+    _textPainter.text = textTextSpan;
+    _sentAtTextPainter.text = sentAtTextSpan;
+    markNeedsLayout();
+  }
+
+  TextDirection get textDirection => _textDirection;
+  set textDirection(TextDirection val) {
+    if (val == _textDirection) {
+      return;
+    }
+    _textDirection = val;
+    _textPainter.textDirection = _textDirection;
+    _sentAtTextPainter.textDirection = _textDirection;
+  }
+
+  //Saved values from performLayout used in paint
+  late bool _sentAtFitsOnLastLine;
+  late double _lineHeight;
+  late double _lastMessageLineWidth;
+  late double _longestLineWidth;
+  late double _sentAtLineWidth;
+  late int _numMessageLines;
+
+  TextSpan get textTextSpan => TextSpan(text: _text, style: _style);
+  TextSpan get sentAtTextSpan => TextSpan(
+        text: _sentAt,
+        style: _style.copyWith(color: Colors.grey),
+      );
+
+  @override
+  void performLayout() {
+    // final childrenSize = child.layout;  <= not required for leaf nodes
+    _textPainter.layout(
+        maxWidth: constraints.maxWidth); //textpainter to know its size
+    final textLines = _textPainter.computeLineMetrics();
+
+    // Repeat for '_sentAtTextPainter'
+    _sentAtTextPainter.layout(maxWidth: constraints.maxWidth);
+    _sentAtLineWidth = _sentAtTextPainter.computeLineMetrics().first.width;
+
+    _longestLineWidth = 0;
+    for (final line in textLines) {
+      _longestLineWidth = max(_longestLineWidth, line.width);
+    }
+    _lastMessageLineWidth = textLines.last.width;
+    _lineHeight = textLines.last.height;
+    _numMessageLines = textLines.length;
+
+    final sizeOfMessage = Size(_longestLineWidth, _textPainter.height);
+
+    // Set '_sentAtFitsOnLastLine'
+    final lastLineWithDate = _lastMessageLineWidth + (_sentAtLineWidth * 1.1);
+
+    if (textLines.length == 1) {
+      _sentAtFitsOnLastLine = lastLineWithDate < constraints.maxWidth;
+    } else {
+      _sentAtFitsOnLastLine =
+          lastLineWithDate < min(_longestLineWidth, constraints.maxWidth);
+    }
+
+    late Size computedSize;
+    if (!_sentAtFitsOnLastLine) {
+      computedSize = Size(
+        sizeOfMessage.width,
+        sizeOfMessage.height + _sentAtTextPainter.height,
+      );
+    } else {
+      if (textLines.length == 1) {
+        computedSize = Size(lastLineWithDate, sizeOfMessage.height);
+      } else {
+        computedSize = Size(_longestLineWidth, sizeOfMessage.height);
+      }
+    }
+    constraints.constrain(computedSize);
+  }
+
+  @override
+  void paint(PaintingContext context, Offset offset) {
+    _textPainter.paint(context.canvas, offset);
+
+    late Offset sentAtOffset;
+    if (_sentAtFitsOnLastLine) {
+      print('x=> ${offset.dx} + ${size.width} - $_sentAtLineWidth');
+      print('y=> ${offset.dy} + $_lineHeight + ${_numMessageLines - 1}');
+      sentAtOffset = Offset(offset.dx + (size.width - _sentAtLineWidth),
+          offset.dy + (_lineHeight * (_numMessageLines - 1)));
+    } else {
+      sentAtOffset = Offset(offset.dx + (size.width - _sentAtLineWidth),
+          offset.dy + (size.height * _numMessageLines));
+    }
+
+    _sentAtTextPainter.paint(context.canvas, sentAtOffset);
+  }
+
+  @override
+  void describeSemanticsConfiguration(SemanticsConfiguration config) {
+    super.describeSemanticsConfiguration(config);
+    config.isSemanticBoundary = true;
+    config.label = '$_text, sent at $_sentAt';
+    config.textDirection = _textDirection;
   }
 }
